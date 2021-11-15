700066d0848556813756c4af817aaae21a7d7056
Michael Harris <xero5k@yahoo.com>
2021-11-14 21:03:22 -0600

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
@@ -6,13 +6,36 @@ function App() {
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  
+  // add event handler for click on button
+  // event handler will toggle betweek light and dark
+  const [isDark, setIsDark] = useState(false);
+  const appClass = isDark ? "App dark" : "App light"
+  
+/*   function ClickHandler(){
+    
+    function clickHandler(){
+      console.log("clicked");
+      setIsDark((isDark) => !isDark)
+      // when clicked, changes background from light to dark
+      // or dark to light
+      //return <button onClick={handle}>Clicked</button>
+    }
+    
+  return <button style={{background: appClass}} onClick={clickHandler}>{isDark ? "Light mode": "Dark mode"}</button>
+} */
+
+function clickHandler(){
+  console.log("clicked");
+  setIsDark((isDark) => !isDark)
+}
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+       <button onClick={clickHandler}>{isDark ? "Light Mode": "Dark Mode"}</button>
+
       </header>
       <ShoppingList items={itemData} />
     </div>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,19 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  const [display, setDisplay] = useState(items)
+  let [selectedCategory, setCategory] = useState("All");
+  const displayItems = display.filter((item)=> {
+    if(selectedCategory === "All"){
+      return true;
+    } else {
+      return item.category === selectedCategory
+    }
+  });
+  //selectedCategory is used to keep 
+ // track of the value from <select>
+ //This is also used to determine which items to display
+
+
+function handleFilter(event){
+  //console.log("Event", className)
+  setCategory(event.target.value)
+  console.log("selectedCategory", selectedCategory)
+  console.log("selectedCategory, category", selectedCategory[0].category)
+}//selectedCategory = items.filter((item)=> item.category === 
+
   return (
-    <div className="ShoppingList">
+    <div className="Shopping List">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" value={selectedCategory[0].category} onChange={handleFilter}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </div>
-      <ul className="Items">
-        {items.map((item) => (
+      <ul className="Items" onChange={handleFilter}>
+        {displayItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
